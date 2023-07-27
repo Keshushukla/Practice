73ef9fc6c980a0d5189c346dc2424d6111205430
Keshu <2003013053@ipec.org.in>
2023-07-27 13:43:58 +0530

    Performing basic operations

--- test.js
+++ test.js
@@ -1,14 +1,44 @@
 let num = 33;
 
 let someObject = Object(num);
-console.log(someObject);
-console.log(typeof someObject);// o/p --> [Number: 33]    object
+// console.log(someObject);
+// console.log(typeof someObject);// o/p --> [Number: 33]    object
 
 let str = "3abc";
 
 let someObject2 = Object(str);
-console.log(someObject2);
-console.log(typeof someObject2);// o/p --> [String: '3abc'] object
+// console.log(someObject2);
+// console.log(typeof someObject2);// o/p --> [String: '3abc'] object
+
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);// str ya num mei jo pehle hota hai o/p usi mei aata hai
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2"); // o/p -->32
+
+console.log( (3 + 4) * 5 % 3);
+
+console.log(+true);
+console.log(+"");
 
 
 
