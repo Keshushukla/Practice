3b8feca4014338adad1bcbb131f2053f048c3265
Keshu <2003013053@ipec.org.in>
2023-07-27 13:24:29 +0530

    Coverting data types to one another

---
+++ test.js
@@ -0,0 +1,14 @@
+let num = 33;
+
+let someObject = Object(num);
+console.log(someObject);
+console.log(typeof someObject);// o/p --> [Number: 33]    object
+
+let str = "3abc";
+
+let someObject2 = Object(str);
+console.log(someObject2);
+console.log(typeof someObject2);// o/p --> [String: '3abc'] object
+
+
+
