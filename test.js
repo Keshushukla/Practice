98cbb675d80ed365a8aff33c87a47b2ef2b9f533
Keshu <2003013053@ipec.org.in>
2023-07-27 13:50:05 +0530

    Performing comparison operations

--- test.js
+++ test.js
@@ -28,17 +28,27 @@ let str1 = "hello"
 let str2 = " hitesh"
 
 let str3 = str1 + str2
-console.log(str3);
+// console.log(str3);
 
-console.log("1" + 2);// str ya num mei jo pehle hota hai o/p usi mei aata hai
-console.log(1 + "2");
-console.log("1" + 2 + 2);
-console.log(1 + 2 + "2"); // o/p -->32
+// console.log("1" + 2);// str ya num mei jo pehle hota hai o/p usi mei aata hai
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2"); // o/p -->32
 
-console.log( (3 + 4) * 5 % 3);
+// console.log( (3 + 4) * 5 % 3);
 
-console.log(+true);
-console.log(+"");
+// console.log(+true);
+// console.log(+"");
 
+// console.log(null > 0);
+// console.log(null == 0);
+//  console.log(null >= 0); >= aur == different way mei perform krte hai . >=n null ko 0 mei convert krta hai
+
+// console.log(undefined === 0);
+// console.log(undefined >= 0);
+// console.log(undefined < 0);
+
+ console.log("2" > 1);
+ console.log("02" > 1);
 
 
