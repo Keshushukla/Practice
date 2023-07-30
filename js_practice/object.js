0c3132da3f2ee01b9d7bc2fcc2b3abeda51d5948
Keshu <2003013053@ipec.org.in>
2023-07-30 12:48:31 +0530

    Accessing Object

--- js_practice/object.js
+++ js_practice/object.js
@@ -1,9 +1,18 @@
-const JsUser = {
+const jsUser = {
     name: "Keshu",
     age: 21,
+    "linkedin":"linkedinId",
     location: "Noida",
     email: "Keshu@google.com",
     isLoggedIn: false,
     lastLoginDays: ["Monday", "Saturday"]
 }
+
+console.log("Accessing Object ------------------");
+
+console.log(jsUser.email);
+console.log(jsUser["email"]);
+
+console.log(jsUser.linkedin);
+console.log(jsUser["linkedin"]);
 
