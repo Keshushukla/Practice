b6eefc32645c4a9c8eff94b9819b68763a80bad5
Keshu <2003013053@ipec.org.in>
2023-07-30 14:48:04 +0530

    Changing Value in Object

--- js_practice/object.js
+++ js_practice/object.js
@@ -1,6 +1,11 @@
+const mySym = Symbol("Key1");
+const mySym2 = Symbol("Key2");
+
 const jsUser = {
     name: "Keshu",
     age: 21,
+    [mySym] : "mykey1",
+    [mySym2] : "mykey2",
     "linkedin":"linkedinId",
     location: "Noida",
     email: "Keshu@google.com",
@@ -15,4 +20,7 @@ console.log(jsUser["email"]);
 
 console.log(jsUser.linkedin);
 console.log(jsUser["linkedin"]);
+
+console.log(jsUser[mySym]);
+console.log(jsUser[mySym2]);
 
