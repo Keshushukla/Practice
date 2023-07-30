b5c9e37322f1f711345afa96835120ddc22bee44
Keshu <2003013053@ipec.org.in>
2023-07-30 11:29:40 +0530

    Understanding Slice & Splice

--- js_practice/array.js
+++ js_practice/array.js
@@ -4,3 +4,14 @@ console.log(arr);
 const myArr = new Array(0,1,2,3,4,5);
 console.log(myArr);
 
+console.log("SLICE ----------");
+
+const arrn1 = myArr.slice(1,3);
+console.log(arrn1);
+console.log(myArr);
+
+console.log("SPLICE ---------");
+
+const arrn2 = myArr.splice(1,3);
+console.log(arrn2);
+console.log(myArr);
