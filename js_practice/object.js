bbbd4d594be0eda418a1a0ee4d6993e207f916e9
Keshu <2003013053@ipec.org.in>
2023-08-02 12:42:50 +0530

    freeze and this  in Object

--- js_practice/object.js
+++ js_practice/object.js
@@ -13,14 +13,32 @@ const jsUser = {
     lastLoginDays: ["Monday", "Saturday"]
 }
 
-console.log("Accessing Object ------------------");
+// console.log("Accessing Object ------------------");
 
-console.log(jsUser.email);
-console.log(jsUser["email"]);
+// console.log(jsUser.email);
+// console.log(jsUser["email"]);
 
-console.log(jsUser.linkedin);
-console.log(jsUser["linkedin"]);
+// console.log(jsUser.linkedin);
+// console.log(jsUser["linkedin"]);
 
-console.log(jsUser[mySym]);
-console.log(jsUser[mySym2]);
+// console.log(jsUser[mySym]);
+// console.log( typeof jsUser[mySym2]);
 
+// console.log(jsUser);
+
+
+// ***********JS Freeze *********
+
+jsUser.email = "keshu@abc.com";
+// console.log(jsUser["email"]);
+
+// Object.freeze(jsUser);
+
+jsUser.email = "keshu@def.com";
+
+jsUser.greeting = function(){
+    console.log(`hello , ${this.name}`);
+}
+
+
+console.log(jsUser.greeting());
