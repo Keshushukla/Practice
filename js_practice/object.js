c44f072c22c739bcbef47c389a5481fa02f81572
Keshu <2003013053@ipec.org.in>
2023-08-11 21:25:47 +0530

    Singleton Object

--- js_practice/object.js
+++ js_practice/object.js
@@ -1,44 +1,56 @@
-const mySym = Symbol("Key1");
-const mySym2 = Symbol("Key2");
+// const mySym = Symbol("Key1");
+// const mySym2 = Symbol("Key2");
 
-const jsUser = {
-    name: "Keshu",
-    age: 21,
-    [mySym] : "mykey1",
-    [mySym2] : "mykey2",
-    "linkedin":"linkedinId",
-    location: "Noida",
-    email: "Keshu@google.com",
-    isLoggedIn: false,
-    lastLoginDays: ["Monday", "Saturday"]
-}
+// const jsUser = {
+//     name: "Keshu",
+//     age: 21,
+//     [mySym] : "mykey1",
+//     [mySym2] : "mykey2",
+//     "linkedin":"linkedinId",
+//     location: "Noida",
+//     email: "Keshu@google.com",
+//     isLoggedIn: false,
+//     lastLoginDays: ["Monday", "Saturday"]
+// }
 
-// console.log("Accessing Object ------------------");
+// // console.log("Accessing Object ------------------");
 
-// console.log(jsUser.email);
-// console.log(jsUser["email"]);
+// // console.log(jsUser.email);
+// // console.log(jsUser["email"]);
 
-// console.log(jsUser.linkedin);
-// console.log(jsUser["linkedin"]);
+// // console.log(jsUser.linkedin);
+// // console.log(jsUser["linkedin"]);
 
-// console.log(jsUser[mySym]);
-// console.log( typeof jsUser[mySym2]);
+// // console.log(jsUser[mySym]);
+// // console.log( typeof jsUser[mySym2]);
 
-// console.log(jsUser);
-
-
-// ***********JS Freeze *********
-
-jsUser.email = "keshu@abc.com";
-// console.log(jsUser["email"]);
-
-// Object.freeze(jsUser);
-
-jsUser.email = "keshu@def.com";
-
-jsUser.greeting = function(){
-    console.log(`hello , ${this.name}`);
-}
+// // console.log(jsUser);
 
 
-console.log(jsUser.greeting());
+// // ***********JS Freeze *********
+
+// jsUser.email = "keshu@abc.com";
+// // console.log(jsUser["email"]);
+
+// // Object.freeze(jsUser);
+
+// jsUser.email = "keshu@def.com";
+
+// jsUser.greeting = function(){
+//     console.log(`hello , ${this.name}`);
+// }
+
+
+// // console.log(jsUser.greeting());
+
+
+
+/**  +++++++++++++++++  CONSTRUCTOR  +++++++++++++++++  */
+
+const tinderUser = new Object()
+
+tinderUser.id = "123abc";
+tinderUser.name = "sam"
+tinderUser.isLoggedIn = "False";
+
+console.log(tinderUser);
