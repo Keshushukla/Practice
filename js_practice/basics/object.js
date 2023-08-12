ba7601b115b5816cc507d189971478253aec0963
Keshu <2003013053@ipec.org.in>
2023-08-13 00:28:40 +0530

    Adding New Files

--- js_practice/basics/object.js
+++ js_practice/basics/object.js
@@ -1,22 +1,22 @@
-// const mySym = Symbol("Key1");
-// const mySym2 = Symbol("Key2");
+const mySym = Symbol("Key1");
+const mySym2 = Symbol("Key2");
 
-// const jsUser = {
-//     name: "Keshu",
-//     age: 21,
-//     [mySym] : "mykey1",
-//     [mySym2] : "mykey2",
-//     "linkedin":"linkedinId",
-//     location: "Noida",
-//     email: "Keshu@google.com",
-//     isLoggedIn: false,
-//     lastLoginDays: ["Monday", "Saturday"]
-// }
+const jsUser = {
+    name: "Keshu",
+    age: 21,
+    [mySym] : "mykey1",
+    [mySym2] : "mykey2",
+    "linkedin":"linkedinId",
+    location: "Noida",
+    email: "Keshu@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
 
 // // console.log("Accessing Object ------------------");
 
 // // console.log(jsUser.email);
-// // console.log(jsUser["email"]);
+console.log(jsUser["email"]);
 
 // // console.log(jsUser.linkedin);
 // // console.log(jsUser["linkedin"]);
