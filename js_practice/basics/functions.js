c352351d3f065031ed9c50fbb918008d4ce9bada
Keshu <2003013053@ipec.org.in>
2023-08-13 20:21:23 +0530

    Rest Operator

--- js_practice/basics/functions.js
+++ js_practice/basics/functions.js
@@ -1,11 +1,22 @@
-function logInUserName(userName){
-if(!userName){
-    console.log("Please enter a name");
-}
-else{
-    console.log(`${userName} just logged in`);
-}
-}
+// function logInUserName(userName){
+// if(!userName){
+//     console.log("Please enter a name");
+// }
+// else{
+//     console.log(`${userName} just logged in`);
+// }
+// }
     
 
-logInUserName()
+// logInUserName()
+
+
+/** +++++REST OPERATOR++++ */
+
+function calcPrice(num1 , num2 , ...num3){
+    console.log(num1);
+    console.log(num2);
+    console.log(num3);
+}
+
+calcPrice(100, 200, 300, 400, 500, 600, 700, 800);
